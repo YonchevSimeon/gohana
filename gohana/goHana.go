41d8d593f587f3081ab85528d88af50b4672d049
Pavel <pavelstefanovv@gmail.com>
2021-09-27 17:34:45 +0300

    updated connect func

--- gohana/goHana.go
+++ gohana/goHana.go
@@ -3,6 +3,7 @@ package gohana
 import (
 	"crypto/tls"
 	"database/sql"
+	"fmt"
 
 	"github.com/SAP/go-hdb/driver"
 )
@@ -22,8 +23,14 @@ func (*Instance) Connect(host, port, username, password string) {
 
 	c.SetTLSConfig(&tlsConfig)
 	Db = sql.OpenDB(c)
+	if err := Db.Ping(); err != nil {
+		fmt.Println("Connection interrupted! Please check your input parameters.")	
+	}else{
+		fmt.Println("Successfully connected!")
+	}
 }
 
 func (*Instance) Disconnect() {
 	Db.Close()
+	fmt.Println("Successfully disconnected!")
 }
